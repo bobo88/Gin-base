562f896298f604263176ec107268d30fa45e8e4f
ycy88 <yuanboi88@163.com>
2026-03-04 15:26:58 +0800

    feat: go mod

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "首页",
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8888") // listen and serve on 0.0.0.0:8080
+}
